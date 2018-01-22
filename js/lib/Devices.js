b0c3ae10d5b0b68e005a8174bf87b8b42164a2c4
brandonsclayton <brandonsclayton@gmail.com>
2018-01-22 10:31:43 -0700

    Devices.js: Artik Cloud devices class

---
+++ js/lib/Devices.js
@@ -0,0 +1,54 @@
+
+
+
+/**
+* @class Devices
+*
+* @clasdesc Class for Artik Cloud devices
+*/
+//............................ Class: Devices ..................................
+class Devices{
+
+  //............................. Constructor ..................................
+  constructor(){
+
+    let _this = this;
+
+    _this.arduinoTemperature = {
+        label: "Arduino Temperature",
+        did: "1497d25089db4a8d84997fd5b2a3d65f",
+        dtid: "dt94cd81097d7f48b3a208a014ca5c1db7",
+    };
+
+    _this.harmonyAC = {
+        label: "Air Conditioner",
+        did: "22190f84bb0845e5a571ab17269f88a4",
+        dtid: "dt26557eba716b4058931840791ac039b0"
+    };
+
+    _this.bedroomLights = [
+        { 
+            label: "Bedroom Ceiling Fan 1",
+            did: "30ad6da22dd94bd896b90c9f61a7c7e8",
+            dtid: "dt6f79b9b4aa3b4a80b7b76c2190016c61"
+        },{
+            label: "Bedroom Ceiling Fan 2",
+            did: "4c507fb2595f4e6f854b976ffcd7c201",
+            dtid: "dt6f79b9b4aa3b4a80b7b76c2190016c61"
+        },{
+            label: "Bedroom Ceiling Fan 3",
+            did: "dbc02b93b20c4f5b98b6344d95f57f36",
+            dtid: "dt6f79b9b4aa3b4a80b7b76c2190016c61"
+        },{
+            label: "Bedroom Ceiling Fan 4",
+            did: "2ae2d82547db42e2ba8cc68eacae3a19",
+            dtid: "dt6f79b9b4aa3b4a80b7b76c2190016c61"
+        }
+    ];
+  
+  
+  }
+  //------------------------- End Constructor ----------------------------------
+
+}
+//---------------------------- End Class: Devices ------------------------------
